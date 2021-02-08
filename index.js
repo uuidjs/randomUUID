71872696fea468ed351b3587b728fa3de73318ab
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2021-02-08 17:28:45 +0100

    Apply polyfill to `crypto.webcrypto` object

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-require('./polyfill')(require('crypto'))
+const crypto = require('crypto')
+
+
+require('./polyfill')(crypto)
+
+let {webcrypto} = crypto
+if(!webcrypto) crypto.webcrypto = webcrypto = {}
+
+if(!webcrypto.randomUUID) webcrypto.randomUUID = crypto.randomUUID
